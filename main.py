1032a9308217938a0405cd2a24af67aa7cc24b7c
Michael Cadiz <mike@cadizm.com>
2017-04-07 10:32:35 -0700

    add prod settings

--- main.py
+++ main.py
@@ -17,9 +17,18 @@ logging.basicConfig(
 if __name__ == '__main__':
     tags = [
         'ootd',
+        'bucketlist',
+        'fashion',
+        'fashionista',
+        'picoftheday',
+        'photography',
+        'wydwt',
+        'style',
+        'styleblogger',
+        'instafashion',
     ]
 
     with closing(InstaBot()) as bot:
         bot.login()
-        usernames = bot.like_tags(tags, num=5)
+        usernames = bot.like_tags(tags, num=100)
         bot.follow_users(random.sample(usernames, len(usernames)/2))
