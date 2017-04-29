c710a70f95da1577cc63b33ca06d9556a1924f2d
Michael Cadiz <mike@cadizm.com>
2017-04-28 21:43:02 -0700

    update like xpath

--- instabot/xpath.py
+++ instabot/xpath.py
@@ -5,5 +5,5 @@ password = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/di
 submit_login = '//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/span/button'
 follow = '//*[@id="react-root"]/section/main/article/header/div[2]/div[1]/span/span[1]/button'
 load_more = '//*[@id="react-root"]/section/main/article/div[2]/a'
-like = '//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/section[2]/a/span'
+like = '//*[@id="react-root"]/section/main/div/div/article/div[2]/section[1]/a[1]/span'
 profile_username = '//*[@id="react-root"]/section/main/div/div[1]/article/header/div/a'
